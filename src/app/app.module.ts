be3649c66e837ebb61f3c626ea8ffdb78491331a
BehruzV <behruzvaliev@gmail.com>
2017-11-18 11:35:32 +0500

    an

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 
 import { AppComponent } from './app.component';
+import { ThemComponent } from './them/them.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatMenuModule,MatButtonModule, MatIconModule, BrowserAnimationsModule,MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/them/them.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-them',
+  templateUrl: './them.component.html',
+  styleUrls: ['./them.component.scss']
+})
+export class ThemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
